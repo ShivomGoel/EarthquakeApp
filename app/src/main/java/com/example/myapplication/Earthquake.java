bd57b0566b77c38340d76c9b239debe34c6fa419
shivom <shivomgoel2@gmail.com>
2021-09-26 11:18:54 +0530

    EarthQuake-App

---
+++ app/src/main/java/com/example/myapplication/Earthquake.java
@@ -0,0 +1,50 @@
+package com.example.myapplication;
+
+public class Earthquake {
+    /** Magnitude of the earthquake */
+    private double mMagnitude;
+
+    /** Location of the earthquake */
+    private String mLocation;
+
+    /** Time of the earthquake */
+    private long mTimeInMilliseconds;
+
+    private String mUrl;
+    /**
+     * Constructs a new {@link Earthquake} object.
+     *
+     * @param magnitude is the magnitude (size) of the earthquake
+     * @param location is the location where the earthquake happened
+     * @param TimeInMilliseconds is the date when the earthquake happened
+     */
+    public Earthquake(double magnitude, String location, long TimeInMilliseconds,String url) {
+        mMagnitude = magnitude;
+        mLocation = location;
+        mTimeInMilliseconds = TimeInMilliseconds;
+        mUrl = url;
+    }
+
+    /**
+     * Returns the magnitude of the earthquake.
+     */
+    public double getMagnitude() {
+        return mMagnitude;
+    }
+
+    /**
+     * Returns the location of the earthquake.
+     */
+    public String getLocation() {
+        return mLocation;
+    }
+
+    /**
+     * Returns the time of the earthquake.
+     */
+    public long getTimeInMilliseconds() {
+        return mTimeInMilliseconds;
+    }
+
+    public String getUrl(){return mUrl;}
+}
